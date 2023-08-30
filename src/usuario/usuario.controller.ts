00f6c1538e7301059302f65553a980034239080c
Marcelo <marcelo.squadra@squara.com.br>
2023-08-30 15:03:04 -0300

    <back>[feat]: criado nova rota para validar email

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common"
+import { Body, Controller, Get, Param, Post } from "@nestjs/common"
 import { CreateUsuarioDto } from "./CreateUsuarioDto"
 import { ResponseUsuarioDto } from "./ResponseUsuarioDto"
 import { UsuarioService } from "./usuario.service"
@@ -13,9 +13,14 @@ export class UsuarioController {
         const usuario = await this.usuarioService.criar(usuarioDto)
         return usuario
     }
+    @Post('validar-email/:email')
+    async validaEmail(@Param('email') email: string): Promise<any> {
+        const emailToken = await this.usuarioService.validarEmail(email)
+        return emailToken
+    }
 
     @Get()
-    async lista() :Promise<any> {
+    async lista(): Promise<any> {
         const usuarios = await this.usuarioService.Listar()
         return usuarios
     }
