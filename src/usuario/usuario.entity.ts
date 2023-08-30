9e5e5eb4948a4f40b00c07a3820926e132a57853
Marcelo <marcelo.squadra@squara.com.br>
2023-08-30 10:11:28 -0300

    <back>[chore]: criada entidade usuario

---
+++ src/usuario/usuario.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column} from "typeorm"
+
+@Entity('usuario')
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  nome: string;
+
+  @Column()
+  sobrenome: string
+
+  @Column({ length: 100 })
+  email: string;
+  
+  @Column()
+  cpf: string;
+
+  @Column()
+  senha: string;
+}
