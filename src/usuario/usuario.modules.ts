7ece74d554d2acf4bb05dfb7d508ab73464f34fd
Marcelo <marcelo.squadra@squara.com.br>
2023-08-31 12:16:39 -0300

    <back>[refactor] implementado tokenservice e entidade token

--- src/usuario/usuario.modules.ts
+++ src/usuario/usuario.modules.ts
@@ -3,10 +3,12 @@ import {TypeOrmModule } from '@nestjs/typeorm'
 import { UsuarioController } from "./usuario.controller";
 import { UsuarioService } from "./usuario.service";
 import { Usuario } from "./usuario.entity";
+import { TokenCadastro } from "src/token/token.cadastro.entity";
+import { TokenService } from "src/token/token.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Usuario])],
+    imports: [TypeOrmModule.forFeature([Usuario, TokenCadastro])],
     controllers: [UsuarioController],
-    providers: [UsuarioService],
+    providers: [UsuarioService, TokenService],
 })
 export class UsuarioModule { }
