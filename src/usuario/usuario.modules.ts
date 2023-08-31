64718b62b9d9e3f3f5cc9d422b9d728f299f6667
Marcelo <marcelo.squadra@squara.com.br>
2023-08-31 17:12:21 -0300

    <back>[feat]: implementação emailService

--- src/usuario/usuario.modules.ts
+++ src/usuario/usuario.modules.ts
@@ -1,14 +1,15 @@
 import { Module } from "@nestjs/common";
-import {TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsuarioController } from "./usuario.controller";
 import { UsuarioService } from "./usuario.service";
 import { Usuario } from "./usuario.entity";
 import { TokenCadastro } from "src/token/token.cadastro.entity";
 import { TokenService } from "src/token/token.service";
+import { MailService } from "src/nodemailer/mail";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Usuario, TokenCadastro])],
     controllers: [UsuarioController],
-    providers: [UsuarioService, TokenService],
+    providers: [UsuarioService, TokenService, MailService],
 })
 export class UsuarioModule { }
