9ea3f9364ac94436ae94845739909c77939f2c49
Marcelo <marcelo.squadra@squara.com.br>
2023-09-04 19:16:38 -0300

    <back>[refactor] implemtentado injeçao inversa  converterString

--- src/endereco/endereco.modules.ts
+++ src/endereco/endereco.modules.ts
@@ -4,12 +4,13 @@ import { Endereco } from "./endereco.entity";
 import { EnderecoController } from "./endereco.controller";
 import { EnderecoService } from "./endereco.service";
 import { BuscarPorCepService } from "src/axios/buscarPorCep";
+import { ConverterStrings, IConverterStrings } from "src/conversores/ConverterString";
 
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Endereco])],
     controllers: [EnderecoController],
-    providers: [EnderecoService, BuscarPorCepService],
+    providers: [{ provide: IConverterStrings, useClass: ConverterStrings }, EnderecoService, BuscarPorCepService, ConverterStrings],
 })
 export class EnderecoModule { }
