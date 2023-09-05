6864709f64978cb5c8b34c25641b92142cabd3a4
Marcelo <marcelo.squadra@squara.com.br>
2023-09-05 14:50:22 -0300

    <back>[feat]: criado uma injeção abstract inversa no pagamentoModules

--- src/pagamento/pagamento.modules.ts
+++ src/pagamento/pagamento.modules.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Pagamento } from "./pagamento.entity";
 import { PagamentoController } from "./pagamento.controller";
 import { PagamentoService } from "./pagamento.service";
+import { IRedesCartoesCreditos, RedesCartoesCreditos } from "./cartao/RedesCartoesCredito";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Pagamento])],
     controllers: [PagamentoController],
-    providers: [PagamentoService],
+    providers: [{ provide: IRedesCartoesCreditos, useClass: RedesCartoesCreditos },
+        RedesCartoesCreditos, PagamentoService],
 })
 export class PagamentoModule { }
