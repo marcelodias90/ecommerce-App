37f862ba826813c48c0e55dd6cfd3fac9e916860
Marcelo <marcelo.squadra@squara.com.br>
2023-09-05 14:49:44 -0300

    <back>[feat]: implementado redesCartoesCreditos em pagamento

--- src/pagamento/pagamento.service.ts
+++ src/pagamento/pagamento.service.ts
@@ -1,4 +1,4 @@
-import {validate} from 'luhn'
+import { validate } from 'luhn'
 import { CartaoInvalidoError } from 'src/erros/CartaoInvalidoError'
 import { CriarPagamentoDto } from './CriarPagamentoDto'
 import { Injectable } from '@nestjs/common'
@@ -6,6 +6,8 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Pagamento } from './pagamento.entity'
 import { Repository } from 'typeorm'
 import { RetornaPagamentoDto } from './RetornaPagamentoDto'
+import { IRedesCartoesCreditos } from './cartao/RedesCartoesCredito'
+import { RedeCartaoNaoPermitidaError } from 'src/erros/RedeCartaoNaoPermitidaError'
 
 @Injectable()
 export class PagamentoService {
@@ -13,13 +15,19 @@ export class PagamentoService {
     constructor(
         @InjectRepository(Pagamento)
         private pagamentoRepository: Repository<Pagamento>,
+        private readonly redesCartoesCreditos: IRedesCartoesCreditos,
     ) { }
 
     async pagamento(dadosPagamento: CriarPagamentoDto, usuario_Id: number): Promise<RetornaPagamentoDto> {
-        const { numeroCartao } = dadosPagamento
-        const cartaoValido = validate(numeroCartao.replace(/\D/g, ''))        
+        let { numeroCartao } = dadosPagamento
+        numeroCartao = numeroCartao.replace(/\D/g, '')       
+        const cartaoValido = validate(numeroCartao)
         if (!cartaoValido) {
             throw new CartaoInvalidoError(numeroCartao)
+        }
+        const redeAceita = this.redesCartoesCreditos.verificarRedeCartao(numeroCartao)
+        if (!redeAceita) {
+            throw new RedeCartaoNaoPermitidaError(numeroCartao)
         }
         const pagamento: Pagamento = this.pagamentoRepository.create({
             numeroCartao: dadosPagamento.numeroCartao,
