0658d86f1508fe72b8299a2dc7efb62f15e6643b
Marcelo <marcelo.squadra@squara.com.br>
2023-09-13 20:49:45 -0300

    <back>[feat]: criado usecases data

---
+++ src/usuario/data/usecases/bd-adicionar-usuario.ts
@@ -0,0 +1,10 @@
+import { AdicionarUsuario } from "src/usuario/domain/usecases/adicionar-usuario";
+import { AdicionarUsuarioRepository } from "../protocols/db/adicionar-usuario";
+
+export class BdAdicionarUsuario implements AdicionarUsuario {
+    constructor(private readonly adicionarUsuarioRepository: AdicionarUsuarioRepository) { }
+
+    async adicionar(data: AdicionarUsuario.Parametros): Promise<AdicionarUsuario.Resultado> {
+        return this.adicionarUsuarioRepository.adicionar(data)
+    }
+}
