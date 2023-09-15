86b8aed43ae31743bd3c2cdc631041a570442fc5
Marcelo <marcelo.squadra@squara.com.br>
2023-09-15 18:00:43 -0300

    <back>[feat]: criado usuarioProvider.ts

---
+++ src/usuario/infra/orm/providers/usuario.provider.ts
@@ -0,0 +1,10 @@
+import { Usuario } from "../entities/usuario.entity";
+import { USUARIO_REPOSITORY } from "../typeOrm/typeorm.repositories";
+
+
+export const usuarioProvider = [
+    {
+        provide: USUARIO_REPOSITORY,
+        useValue: Usuario
+    }
+]
