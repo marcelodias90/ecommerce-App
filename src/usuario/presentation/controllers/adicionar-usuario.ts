6fcb0120b3db6078b832d9fd3e0103d8333fcbc7
Marcelo <marcelo.squadra@squara.com.br>
2023-09-26 16:53:42 -0300

    <back>[refactor]: mudança no AdicionarUsuarioController

--- src/usuario/presentation/controllers/adicionar-usuario.ts
+++ src/usuario/presentation/controllers/adicionar-usuario.ts
@@ -1,20 +1,45 @@
 import { Controller } from "../protocols/controller";
 import { HttpResponse } from "../protocols/http";
 import { UsuarioModel } from "src/usuario/domain/entities/usuario";
-import { createdSuccess, serverError } from "../helpers/http.helpers"
+import { badRequest, createdSuccess, serverError } from "../helpers/http.helpers"
 import { AdicionarUsuario } from "../../domain/usecases/adicionar-usuario";
+import { BuscarPorEmail } from "src/usuario/domain/usecases/buscar-por-email";
+import { EmailExistenteError } from "src/erros/EmailExistenteError";
+import { STATUS } from "../../data/enums/usuarioStatus.enum"
 
 export class AdicionarUsuarioController implements Controller {
-    constructor(private readonly adicionarUsuario: AdicionarUsuario) { }
-    async lidar(request: any): Promise<HttpResponse> {
-    try {
-            const usuario: UsuarioModel = {
-                ...request
-            }
+    constructor(
+        private readonly adicionarUsuario: AdicionarUsuario,
+        private readonly buscarPorEmail: BuscarPorEmail,
+    ) { }
+    async lidar(parametros: AdicionarUsuarioController.Parametros): Promise<HttpResponse> {
+        try {
+            const { email } = parametros
+
+            const emailExiste = await this.buscarPorEmail.buscarEmail({ email })
+            if (emailExiste) return badRequest(new EmailExistenteError(email))
+
+            const usuario = {
+                ...parametros,
+                status: STATUS.DESATIVADO
+            } as UsuarioModel
+
             const novoUsuario = await this.adicionarUsuario.adicionar(usuario);
             return createdSuccess(novoUsuario)
-        }catch(erro) {
+        } catch (erro) {
+            console.log(erro);
             return serverError(erro)
         }
+    }
+}
+
+export namespace AdicionarUsuarioController {
+    export type Parametros = {
+        nome: string
+        sobrenome: string
+        email: string;
+        cpf: string;
+        senha: string;
+        status: number;
     }
 }
