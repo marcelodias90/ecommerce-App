9241ad47c83c14c5af110d26aca90ed962434b55
Marcelo <marcelo.squadra@squara.com.br>
2023-09-26 16:47:04 -0300

    <back>[refactor]: mudança no usuarioController

--- src/usuario/main/controllers/usuario/usuario.controller.ts
+++ src/usuario/main/controllers/usuario/usuario.controller.ts
@@ -1,25 +1,27 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common"
+import { Body, Controller, Get, Param, Post , Res} from "@nestjs/common"
 import { CriarUsuarioDto } from "../../../CriarUsuarioDto"
-import { RetornaUsuarioDto } from "../../../RetornaUsuarioDto"
-import { UsuarioService } from "../../../infra/orm/repositories/usuario.service"
-
+import { HttpResponse } from "src/usuario/presentation/protocols/http"
+import { ConstruirAdicionarUsuarioController } from "../../factories/controllers/adicionar-usuario.factory"
+import { controllerAdpter } from "../../adpters/controller.adapter"
 
 @Controller('usuario')
 export class UsuarioController {
-    constructor(private readonly usuarioService: UsuarioService) { }
+    constructor(
+        private readonly construirAdicionarUsuarioController: ConstruirAdicionarUsuarioController,
+    ) { }
 
-    @Post('criar')
-    async cria(@Body() usuarioDto: CriarUsuarioDto): Promise<RetornaUsuarioDto> {
-        return await this.usuarioService.criar(usuarioDto)
+    @Post('adiciona')
+    async cria(@Body() body: CriarUsuarioDto,  @Res() res: any,): Promise<HttpResponse> {        
+        const resultado = await controllerAdpter(this.construirAdicionarUsuarioController.fabricar(), body)
+        return res.status(resultado.statusCode).json(resultado)
+    }
+    // @Post('validar-email/:email')
+    // async validaEmail(@Param('email') email: string): Promise<void> {
+    //     await this.usuarioService.validarEmail(email)
+    // }
 
-    }
-    @Post('validar-email/:email')
-    async validaEmail(@Param('email') email: string): Promise<void> {
-        await this.usuarioService.validarEmail(email)
-    }
-
-    @Get()
-    async lista(): Promise<RetornaUsuarioDto[]> {
-        return await this.usuarioService.listar()
-    }
+    // @Get()
+    // async lista(): Promise<RetornaUsuarioDto[]> {
+    //     return await this.usuarioService.listar()
+    // }
 }
