4502f9e1ce84a2ae137c128421ba5e41cf3af36f
Marcelo <marcelo.squadra@squara.com.br>
2023-09-26 16:50:52 -0300

    <back>[feat]: criado Bd buscarPorEmai

--- src/usuario/data/usecases/bd-adicionar-usuario.ts
+++ src/usuario/data/usecases/bd-adicionar-usuario.ts
@@ -1,5 +1,5 @@
 import { AdicionarUsuario } from "src/usuario/domain/usecases/adicionar-usuario";
-import { AdicionarUsuarioRepository } from "../protocols/db/adicionar-usuario";
+import { AdicionarUsuarioRepository } from "../protocols/db/adicionar-usuario/adicionar-usuario";
 
 export class BdAdicionarUsuario implements AdicionarUsuario {
     constructor(private readonly adicionarUsuarioRepository: AdicionarUsuarioRepository) { }
