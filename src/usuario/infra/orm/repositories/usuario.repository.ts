beecb61f4ca8cb910bef31dd0422af41ec5e5c84
Marcelo <marcelo.squadra@squara.com.br>
2023-09-26 16:52:07 -0300

    <back>[feat]: adicionado novo UseCases

--- src/usuario/infra/orm/repositories/usuario.repository.ts
+++ src/usuario/infra/orm/repositories/usuario.repository.ts
@@ -1,13 +1,15 @@
 import { Inject, Injectable } from "@nestjs/common";
 
-import { AdicionarUsuarioRepository } from "src/usuario/data/protocols/db/adicionar-usuario";
+import { AdicionarUsuarioRepository } from "src/usuario/data/protocols/db/adicionar-usuario/adicionar-usuario";
 import { AdicionarUsuario } from "src/usuario/domain/usecases/adicionar-usuario";
 import { Repository } from "typeorm";
 import { Usuario } from "../entities/usuario.entity";
 import { USUARIO_REPOSITORY } from "../typeOrm/typeorm.repositories";
+import { BuscarPorEmailRepository } from "src/usuario/data/protocols/db/buscar-email/buscar-por-email";
+import { BuscarEmail } from "src/usuario/domain/usecases/buscar-por-email";
 
 @Injectable()
-export class UsuarioRepository implements AdicionarUsuarioRepository {
+export class UsuarioRepository implements AdicionarUsuarioRepository, BuscarPorEmailRepository {
     constructor(@Inject(USUARIO_REPOSITORY) private usuarioRepository: Repository<Usuario>) { }
     async adicionar(data: AdicionarUsuario.Parametros): Promise<AdicionarUsuario.Resultado> {
         const usuario = await this.usuarioRepository.save(data)
@@ -18,4 +20,12 @@ export class UsuarioRepository implements AdicionarUsuarioRepository {
             email: usuario.email
         }
     }
+
+    async buscarEmail(data: BuscarEmail.Parametros): Promise<BuscarEmail.Resultado> {
+        const { email } = data
+        return await this.usuarioRepository.findOne({
+            where: { email }
+        })
+    }
+
 } 
