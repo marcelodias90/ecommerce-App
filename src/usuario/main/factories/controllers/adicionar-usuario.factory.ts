63f0903e6ef081ea2bda82c8f751641633b6901b
Marcelo <marcelo.squadra@squara.com.br>
2023-09-26 16:48:31 -0300

    <back>[feat]: adicionado novo providers

--- src/usuario/main/factories/controllers/adicionar-usuario.factory.ts
+++ src/usuario/main/factories/controllers/adicionar-usuario.factory.ts
@@ -2,15 +2,19 @@ import { Inject, Injectable } from "@nestjs/common";
 import { AdicionarUsuario } from "../../../domain/usecases/adicionar-usuario";
 import { AdicionarUsuarioController } from "../../../presentation/controllers/adicionar-usuario";
 import { Controller } from "src/usuario/presentation/protocols/controller";
-import { ADICIONAR_USUARIO_FACTORY } from "../providers";
+import { ADICIONAR_USUARIO_FACTORY, BUSCAR_POR_EMAIL_FACTORY } from "../providers";
 import { LogControllerDecoratorFactory } from "./log-controller-decorator.factory";
+import { BuscarPorEmail } from "src/usuario/domain/usecases/buscar-por-email";
 
 @Injectable()
 export class ConstruirAdicionarUsuarioController {
-    constructor(@Inject(ADICIONAR_USUARIO_FACTORY) private readonly adicionarUsuario: AdicionarUsuario) { }
+    constructor(
+        @Inject(ADICIONAR_USUARIO_FACTORY) private readonly adicionarUsuario: AdicionarUsuario,
+        @Inject(BUSCAR_POR_EMAIL_FACTORY) private readonly buscarPorEmail: BuscarPorEmail
+        ) { }
 
     public fabricar(): Controller {
-        const controller = new AdicionarUsuarioController(this.adicionarUsuario)
+        const controller = new AdicionarUsuarioController(this.adicionarUsuario, this.buscarPorEmail)
         return new LogControllerDecoratorFactory(controller)
     }
 }
