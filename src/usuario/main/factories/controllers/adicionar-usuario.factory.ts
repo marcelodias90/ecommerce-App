773cf3845ff71e15b4ff4e3fc9243254abe3ae2e
Marcelo <marcelo.squadra@squara.com.br>
2023-09-26 12:26:01 -0300

    <back>[feat]: adicionado logControllerDecoretor

--- src/usuario/main/factories/controllers/adicionar-usuario.factory.ts
+++ src/usuario/main/factories/controllers/adicionar-usuario.factory.ts
@@ -1,16 +1,16 @@
-import { Inject } from "@nestjs/common";
-import { AdicionarUsuario } from "src/usuario/domain/usecases/adicionar-usuario";
-import { adicionarUsuarioController } from "src/usuario/presentation/controllers/adicionar-usuario";
+import { Inject, Injectable } from "@nestjs/common";
+import { AdicionarUsuario } from "../../../domain/usecases/adicionar-usuario";
+import { AdicionarUsuarioController } from "../../../presentation/controllers/adicionar-usuario";
 import { Controller } from "src/usuario/presentation/protocols/controller";
 import { ADICIONAR_USUARIO_FACTORY } from "../providers";
+import { LogControllerDecoratorFactory } from "./log-controller-decorator.factory";
 
-
+@Injectable()
 export class ConstruirAdicionarUsuarioController {
-
-    constructor (@Inject(ADICIONAR_USUARIO_FACTORY) private readonly adicionarUsuario: AdicionarUsuario ){}
+    constructor(@Inject(ADICIONAR_USUARIO_FACTORY) private readonly adicionarUsuario: AdicionarUsuario) { }
 
     public fabricar(): Controller {
-        const controller = new adicionarUsuarioController(this.adicionarUsuario)
-        return controller
+        const controller = new AdicionarUsuarioController(this.adicionarUsuario)
+        return new LogControllerDecoratorFactory(controller)
     }
 }
